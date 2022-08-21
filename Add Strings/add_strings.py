4e5459f3e3e2efcaff9f3c681d2ee46d5e986d91
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-21 19:05:51 -0400

    Solution

---
+++ Add Strings/add_strings.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        num1 = int(num1)
+        num2 = int(num2)
+        math = num1 + num2 
+        return str(math)
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        
