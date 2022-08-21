c6732f79a3b3500047a5d66c7f5eb345243cddf1
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-21 18:44:16 -0400

    Update Palindrome_number.py

--- Palindrome_number.py
+++ Palindrome_number.py
@@ -1,7 +1,18 @@
 class Solution(object):
     def isPalindrome(self, x):
+        start = str(x)
+        string = ""
+        for letter in start:
+            string += letter
+        print(string)
+
+
+
+
         """
         :type x: int
         :rtype: bool
         """
-        
+# Test
+test = Solution()
+test.isPalindrome(123)
