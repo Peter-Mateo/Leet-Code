a139a7356f9269c14fe5f054ce1a0455512859b0
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-21 18:59:23 -0400

    check

---
+++ Palindrome Number/Palindrome_number.js
@@ -0,0 +1,10 @@
+/**
+    * @param {number} x
+     * @return {boolean}
+    */
+var isPalindrome = function(x) {
+    function x(x){
+        console.log(x);
+    };
+};
+console.log(isPalindrome);
