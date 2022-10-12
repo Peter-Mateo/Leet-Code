86f7848a23c8d2b68d0e0a6f0064b1ab76ba9308
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-10-12 17:06:33 -0400

    practice

---
+++ Two Sum/test.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class test {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        int[] nums = {3,2,4};
+        int target = 6;
+        System.out.println(Arrays.toString(solution.twoSum(nums, target)));
+    }
+}
